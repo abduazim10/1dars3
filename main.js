faf7431b0f1abc77b3c9a13575e60a40e9f02c15
abduazim10 <abduazimit@gmail.com>
2024-03-22 14:49:56 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,82 @@
+// let weather = prompt()
+
+// switch(weather){
+//     case 'bulutli':
+//         console.log('Bugun Yomgir yogadi');
+//         break;
+//     case 'quyoshli':
+//         console.log('Bugun Havo ochiq');
+//         break;
+//     case 'qor':
+//         console.log('bugun havo soviq');
+//         break;
+//     default:
+//         console.log('Bunday malumot yoq');
+//         break;
+// }
+
+let meva = prompt('1dan 7gacha son kiriting')
+
+switch(meva){
+    case 1:
+        alert('Dushanba');
+        break;
+    case 2:
+        alert('Seshanba');
+        break;
+    case 3:
+        alert('Chorshanba');
+        break;
+        case 4:
+            alert('Payshanba');
+        break;
+        case 5:
+            alert('Juma');
+        break;
+        case 6:
+        alert('shanba');
+        break;
+        case 7:
+        alert('yakshanba');
+        break;
+    default:
+        alert('1haftada 7kun bor');
+        break;
+}
+    // let meva = prompt().toLowerCase()
+
+    // switch(meva){
+    //     case '1':
+    //         console.log('bir');
+    //         break;
+    //     case '2':
+    //         console.log('ikki');
+    //         break;
+    //     case '3':
+    //         console.log('uch');
+    //         break;
+    //     case '4':
+    //         console.log('tort');
+    //         break;
+    //     case '5':
+    //         console.log('besh');
+    //         break;
+    //     case '6':
+    //         console.log('olti');
+    //         break;
+    //     case '7':
+    //         console.log('yetti');
+    //         break;
+    //     case '8':
+    //         console.log('sakkiz');
+    //         break;
+    //     case '9':
+    //         console.log('toqqiz');
+    //         break;
+    //     case '10':
+    //         console.log('on');
+    //         break;
+    //     default:
+    //         console.log('Bunday son yoq');
+    //         break;
+    // }
